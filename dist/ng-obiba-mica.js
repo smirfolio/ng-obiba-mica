f9867196bab7d72168fcdd32666658a60020db1b
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-11-30 16:42:41 -0500

    Added a setter for url provider

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -44,36 +44,42 @@ angular.module('ngObibaMica', [
   'obiba.mica.utils',
   'obiba.mica.access'
 ])
+.config(['$provide', function($provide) {
+  $provide.provider('ngObibaMicaUrl', function() {
+    var registry = {
+      'DataAccessFormConfigResource': 'ws/config/data-access-form',
+      'DataAccessRequestsResource': 'ws/data-access-requests',
+      'DataAccessRequestResource': 'ws/data-access-request/:id',
+      'DataAccessRequestCommentsResource': 'ws/data-access-request/:id/comments',
+      'DataAccessRequestCommentResource': 'ws/data-access-request/:id/comment/:commentId',
+      'DataAccessRequestStatusResource': 'ws/data-access-request/:id/_status?to=:status',
+    };
 
-  .config(['$provide', function($provide) {
-    $provide.provider('ngObibaMicaUrl', function() {
-      var registry = {
-        'DataAccessFormConfigResource': 'ws/config/data-access-form',
-        'DataAccessRequestsResource': 'ws/data-access-requests',
-        'DataAccessRequestResource': 'ws/data-access-request/:id',
-        'DataAccessRequestCommentsResource': 'ws/data-access-request/:id/comments',
-        'DataAccessRequestCommentResource': 'ws/data-access-request/:id/comment/:commentId',
-        'DataAccessRequestStatusResource': 'ws/data-access-request/:id/_status?to=:status',
+    function UrlProvider(registry) {
+      var urlRegistry = registry;
+
+      this.getUrl =function(resource) {
+        if (resource in urlRegistry) {
+          return urlRegistry[resource];
+        }
+
+        return null;
       };
+    }
 
-      function UrlProvider() {
-
-        this.getUrl =function(resource) {
-          if (resource in registry) {
-            return registry[resource];
-          }
-
-          return null;
-        };
+    this.setUrl = function(key, url) {
+      if (key in registry) {
+        registry[key] = url;
       }
+    };
 
-      this.$get = function() {
-        return new UrlProvider();
-      };
+    this.$get = function() {
+      return new UrlProvider(registry);
+    };
 
-    });
+  });
 
-  }]);
+}]);
 
 ;/*
  * Copyright (c) 2014 OBiBa. All rights reserved.
