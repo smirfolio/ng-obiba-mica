bdc7c6be94f49ad3224dae1060e99f99e391de55
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-12-02 13:54:36 -0500

    Added USER_ROLES

--- src/ng-obiba-mica.js
+++ src/ng-obiba-mica.js
@@ -1,7 +1,11 @@
 'use strict';
 
 
-angular.module('ngObibaMica')
+angular.module('ngObibaMica', [
+  'schemaForm',
+  'obiba.mica.utils',
+  'obiba.mica.access'
+])
   .constant('USER_ROLES', {
     all: '*',
     admin: 'mica-administrator',
@@ -9,48 +13,42 @@ angular.module('ngObibaMica')
     editor: 'mica-editor',
     user: 'mica-user',
     dao: 'mica-data-access-officer'
-  });
+  })
 
-
-angular.module('ngObibaMica', [
-  'schemaForm',
-  'obiba.mica.utils',
-  'obiba.mica.access'
-])
-.config(['$provide', function($provide) {
-  $provide.provider('ngObibaMicaUrl', function() {
-    var registry = {
-      'DataAccessFormConfigResource': 'ws/config/data-access-form',
-      'DataAccessRequestsResource': 'ws/data-access-requests',
-      'DataAccessRequestResource': 'ws/data-access-request/:id',
-      'DataAccessRequestCommentsResource': 'ws/data-access-request/:id/comments',
-      'DataAccessRequestCommentResource': 'ws/data-access-request/:id/comment/:commentId',
-      'DataAccessRequestStatusResource': 'ws/data-access-request/:id/_status?to=:status',
-    };
-
-    function UrlProvider(registry) {
-      var urlRegistry = registry;
-
-      this.getUrl =function(resource) {
-        if (resource in urlRegistry) {
-          return urlRegistry[resource];
-        }
-
-        return null;
+  .config(['$provide', function($provide) {
+    $provide.provider('ngObibaMicaUrl', function() {
+      var registry = {
+        'DataAccessFormConfigResource': 'ws/config/data-access-form',
+        'DataAccessRequestsResource': 'ws/data-access-requests',
+        'DataAccessRequestResource': 'ws/data-access-request/:id',
+        'DataAccessRequestCommentsResource': 'ws/data-access-request/:id/comments',
+        'DataAccessRequestCommentResource': 'ws/data-access-request/:id/comment/:commentId',
+        'DataAccessRequestStatusResource': 'ws/data-access-request/:id/_status?to=:status',
       };
-    }
 
-    this.setUrl = function(key, url) {
-      if (key in registry) {
-        registry[key] = url;
+      function UrlProvider(registry) {
+        var urlRegistry = registry;
+
+        this.getUrl =function(resource) {
+          if (resource in urlRegistry) {
+            return urlRegistry[resource];
+          }
+
+          return null;
+        };
       }
-    };
 
-    this.$get = function() {
-      return new UrlProvider(registry);
-    };
+      this.setUrl = function(key, url) {
+        if (key in registry) {
+          registry[key] = url;
+        }
+      };
 
-  });
+      this.$get = function() {
+        return new UrlProvider(registry);
+      };
 
-}]);
+    });
+
+  }]);
 
