e904d60c3df9b5eb522da803ee3852d77835ee27
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2016-02-03 17:42:41 -0500

    MK-644 - added criteria builder

--- src/search/search-service.js
+++ src/search/search-service.js
@@ -10,6 +10,227 @@
 
 'use strict';
 
+/**
+ * Visitor used to walk the JSON query and return a node matching input name
+ * @param name
+ * @constructor
+ */
+var FindNodeVisitor = function (name) {
+  this.node = null;
+  this.name = name;
+};
+
+/**
+ * Returns TRUE if node is found
+ * @param node
+ * @returns {boolean}
+ */
+FindNodeVisitor.prototype.accept = function (node) {
+
+  if (this.name === node.name) {
+    this.node = node;
+    return true;
+  }
+  // must go on!
+  return false;
+};
+
+/**
+ * Returns the found node while traversing JSON
+ * @returns {null|*}
+ */
+FindNodeVisitor.prototype.getNode = function () {
+  return this.node;
+};
+
+/**
+ * Abstract class for all criteria builders
+ * @param root
+ * @param taxonomies
+ * @param createCriteriaCallback
+ * @param lang
+ * @constructor
+ */
+function AbstractCriteriaBuilder(root, taxonomies, createCriteriaCallback, lang) {
+  this.target = null;
+  this.root = root;
+  this.taxonomies = taxonomies;
+  this.criteriaList = [];
+  this.createCriteriaCallback = createCriteriaCallback;
+  this.lang = lang;
+
+  /**
+   * Called by the leaf visitor to create a criteria
+   * @param targetTaxonomy
+   * @param targetVocabulary
+   * @param targetTerms
+   */
+  this.addCriteria = function(targetTaxonomy, targetVocabulary, targetTerms) {
+    var that = this;
+
+    var foundTaxonmy = this.taxonomies.filter(function(taxonomy){
+      return targetTaxonomy === taxonomy.name;
+    });
+
+    if (foundTaxonmy.length === 0 ) {
+      throw new Error('Could not find taxonomy:', targetTaxonomy);
+    }
+
+    foundTaxonmy = foundTaxonmy[0];
+
+    var foundVocabulary = foundTaxonmy.vocabularies.filter(function(vocabulary){
+      return targetVocabulary === vocabulary.name;
+    });
+
+    if (foundVocabulary.length === 0 ) {
+      throw new Error('Could not find vocabulary:', targetVocabulary);
+    }
+
+    var foundCount = 0;
+    foundVocabulary = foundVocabulary[0];
+
+    if (foundVocabulary.terms) {
+      foundVocabulary.terms.some(function (term) {
+        if (targetTerms.indexOf(term.name) !== -1) {
+          that.criteriaList.push(that.createCriteriaCallback(that.target, foundTaxonmy, foundVocabulary, term, that.lang));
+          foundCount++;
+
+          // stop searching
+          return foundCount === targetTerms.length;
+        }
+      });
+    }
+    else {
+      that.criteriaList.push(that.createCriteriaCallback(that.target, foundTaxonmy, foundVocabulary));
+    }
+  };
+}
+
+/**
+ * This method is where a criteria gets created
+ */
+AbstractCriteriaBuilder.prototype.visitLeaf = function (/*node*/) {
+
+};
+
+/**
+ * Returns all the criterias found
+ * @returns {Array}
+ */
+AbstractCriteriaBuilder.prototype.getCriteriaList = function (/*node*/) {
+  return this.criteriaList;
+};
+
+/**
+ * Node condition visitor
+ * @param node
+ */
+AbstractCriteriaBuilder.prototype.visitCondition =function (node) {
+  this.visit(node.args[0]);
+  this.visit(node.args[1]);
+};
+
+/**
+ * General purpose node visitor
+ * @param node
+ */
+AbstractCriteriaBuilder.prototype.visit = function(node) {
+  console.log('Visit', node);
+
+  switch (node.name) {
+    case 'and':
+    case 'or':
+      this.visitCondition(node);
+      break;
+    case 'in':
+    case 'out':
+    case 'eq':
+    case 'le':
+    case 'lt':
+    case 'ge':
+    case 'gt':
+    case 'between':
+      this.visitLeaf(node);
+      break;
+    case 'not':
+    case 'match':
+      break;
+    default:
+  }
+};
+
+/**
+ * Builds a criteria list for this target
+ */
+AbstractCriteriaBuilder.prototype.build = function() {
+  var that = this;
+  this.root.args.forEach(function(node){
+    that.visit(node);
+  });
+};
+
+/**
+ * Variable criteria builder
+ * @param root
+ * @param taxonomies
+ * @param createCriteriaCallback
+ * @param lang
+ * @constructor
+ */
+
+var VariableCriteriaBuilder = function(root, taxonomies, createCriteriaCallback, lang) {
+  AbstractCriteriaBuilder.call(this, root, taxonomies, createCriteriaCallback, lang);
+  this.target = 'variable';
+
+  this.parserFieldName = function(name) {
+    var re = /(\w+)__(\w+)/;
+    var m = re.exec(name.replace(/attributes\./, '').replace(/\.\w+$/,''));
+
+    if (m && m.length > 2) {
+      return {taxonomy: m[1], vocabulary: m[2]};
+    }
+
+    throw new Error('Invalid field name', name);
+  };
+};
+
+VariableCriteriaBuilder.prototype = Object.create(AbstractCriteriaBuilder.prototype);
+
+VariableCriteriaBuilder.prototype.visitLeaf = function(node) {
+  console.log('VariableCriteriaBuilder');
+  var field = node.args[0];
+  var values = node.args[1];
+  var searchInfo = this.parserFieldName(field);
+  console.log(node.name, 'F:', field, 'V:', values);
+
+  this.addCriteria(searchInfo.taxonomy, searchInfo.vocabulary, values instanceof Array ? values : [values]);
+};
+
+/**
+ * TODO Given a target node, it adds a RQLQuery
+ * @param parser
+ * @constructor
+ */
+
+var RqlQueryBuilder = function(parser) {
+  this.rqlParser = parser;
+};
+
+/**
+ * TODO implement real code...
+ * @param name
+ * @param field
+ * @param args
+ * @returns {*}
+ */
+RqlQueryBuilder.prototype.node = function(name, field, args) {
+  var q = ':name(:f,(:args))'.replace(/:name/,name).replace(/:f/, field).replace(/:args/, args.join(','));
+  return this.rqlParser.parse(q).args[0];
+};
+
+/**
+ * Module services and factories
+ */
 angular.module('obiba.mica.search')
   .factory('TaxonomiesSearchResource', ['$resource', 'ngObibaMicaUrl',
     function ($resource, ngObibaMicaUrl) {
@@ -42,7 +263,6 @@ angular.module('obiba.mica.search')
         }
       });
     }])
-
 
   .factory('JoinQuerySearchResource', ['$resource', 'ngObibaMicaUrl',
     function ($resource, ngObibaMicaUrl) {
@@ -89,6 +309,7 @@ angular.module('obiba.mica.search')
         }
       });
     }])
+
   .service('ObibaSearchConfig', function () {
     var options = {
       networks: null,
@@ -111,4 +332,149 @@ angular.module('obiba.mica.search')
       return angular.copy(options);
     };
 
-  });
+  })
+
+  /**
+   * Service providing RQL related functionality
+   */
+  .service('RqlQueryService', [
+    '$q',
+    'TaxonomiesResource',
+    'LocalizedValues',
+    function ($q, TaxonomiesResource, LocalizedValues) {
+      // TODO add the rest of the target taxonomy caches
+      var variableTaxonomies = null;
+
+      /**
+       * Walks through the JSON query and calls a visitor
+       * @param node
+       * @param visitor
+       * @returns {boolean}
+       */
+      function walk(node, visitor) {
+        console.log('findNode', node);
+        if (node instanceof RqlQuery) {
+          //var that = this;
+
+          if (visitor.accept(node)) {
+            return true;
+          } else if (node.args) {
+            node.args.some(function (child) {
+              return walk(child, visitor);
+            });
+          }
+        }
+        return false;
+      }
+
+      /**
+       * Finds a query node by name
+       * @param root
+       * @param name
+       * @returns {null|*}
+       */
+      function findNode(root, name) {
+        var visitor = new FindNodeVisitor(name);
+        walk(root, visitor);
+        return visitor.getNode();
+      }
+
+      /**
+       * Creates a criteria item
+       * @param target
+       * @param taxonomy
+       * @param vocabulary
+       * @param term
+       * @param lang
+       * @returns {{id: string, taxonomy: *, vocabulary: *, term: *, target: *, lang: *, itemTitle: string, itemDescription: string, itemParentTitle: string, itemParentDescription: string}}
+       */
+      this.createCriteria = function(target, taxonomy, vocabulary, term, lang) {
+        var id = taxonomy.name + '::' + vocabulary.name;
+        if (term) {
+          id = id + ':' + term.name;
+        }
+        var criteria = {
+          id: id,
+          taxonomy: taxonomy,
+          vocabulary: vocabulary,
+          term: term,
+          target: target,
+          lang: lang,
+          itemTitle: '',
+          itemDescription: '',
+          itemParentTitle: '',
+          itemParentDescription: ''
+        };
+
+        // prepare some labels for display
+        if(term) {
+          criteria.itemTitle = LocalizedValues.forLocale(term.title, lang);
+          criteria.itemDescription = LocalizedValues.forLocale(term.description,lang);
+          criteria.itemParentTitle = LocalizedValues.forLocale(vocabulary.title, lang);
+          criteria.itemParentDescription = LocalizedValues.forLocale(vocabulary.description, lang);
+          if (!criteria.itemTitle) {
+            criteria.itemTitle = term.name;
+          }
+          if (!criteria.itemParentTitle) {
+            criteria.itemParentTitle = vocabulary.name;
+          }
+        } else {
+          criteria.itemTitle = LocalizedValues.forLocale(vocabulary.title, lang);
+          criteria.itemDescription = LocalizedValues.forLocale(vocabulary.description, lang);
+          criteria.itemParentTitle = LocalizedValues.forLocale(taxonomy.title, lang);
+          criteria.itemParentDescription = LocalizedValues.forLocale(taxonomy.description, lang);
+          if (!criteria.itemTitle) {
+            criteria.itemTitle = vocabulary.name;
+          }
+          if (!criteria.itemParentTitle) {
+            criteria.itemParentTitle = taxonomy.name;
+          }
+        }
+
+        return criteria;
+      };
+
+      /**
+       * Finds the variable query node
+       * @param root
+       * @returns {null|*}
+       */
+      this.variableNode = function (root) {
+        return findNode(root, 'variable');
+      };
+
+      /**
+       * Builds the variable query criterias
+       * @param root
+       * @param lang
+       * @returns {*}
+       */
+      this.buildVariableCriteria = function(root, lang) {
+        var that = this;
+        var deferred = $q.defer();
+
+        function build() {
+          var variableNode = that.variableNode(root);
+          if (variableNode) {
+            var builder = new VariableCriteriaBuilder(variableNode, that.variableTaxonomies, that.createCriteria, lang);
+            builder.build();
+            console.log('Criterias:', builder.getCriteriaList());
+            deferred.resolve(builder.getCriteriaList());
+          }
+        }
+
+        if (variableTaxonomies) {
+          build();
+        } else {
+          TaxonomiesResource.get({
+            target: 'variable'
+          }).$promise.then(function(response) {
+            that.variableTaxonomies = response;
+            build();
+          });
+        }
+
+        return deferred.promise;
+      };
+
+    }]);
