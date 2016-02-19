3cdd5c24526ff13ec61140ac368c3e405db03cd7
Yannick Marcon <yannick.marcon@obiba.org>
2016-02-19 17:29:55 -0500

    Some translations

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -3308,10 +3308,11 @@ angular.module('obiba.mica.search')
 
   .controller('CriterionDropdownController', [
     '$scope',
+    '$filter',
     'LocalizedValues',
     'RqlQueryUtils',
     'StringUtils',
-    function ($scope, LocalizedValues, RqlQueryUtils, StringUtils) {
+    function ($scope, $filter, LocalizedValues, RqlQueryUtils, StringUtils) {
       var closeDropdown = function () {
         if (!$scope.state.open) {
           return;
@@ -3362,10 +3363,10 @@ angular.module('obiba.mica.search')
         var operation = rqlQuery.name;
         switch (rqlQuery.name) {
           case RQL_NODE.EXISTS:
-            operation = ':any'; //TODO translate
+            operation = ':' + $filter('translate')('any');
             break;
           case RQL_NODE.MISSING:
-            operation = ':none'; //TODO translate
+            operation = ':' + $filter('translate')('none');
             break;
           case RQL_NODE.EQ:
             operation = '=' + rqlQuery.args[1];
@@ -3377,7 +3378,7 @@ angular.module('obiba.mica.search')
             operation = '<' + rqlQuery.args[1];
             break;
           case RQL_NODE.BETWEEN:
-            operation = ':[' + rqlQuery.args[1] + '[';
+            operation = ':[' + rqlQuery.args[1] + ')';
             break;
           case RQL_NODE.IN:
             operation = '';
