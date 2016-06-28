b46c0332e49d847e5193b5fd4e0ff96ac0b8b19e
Yannick Marcon <yannick.marcon@obiba.org>
2016-06-28 13:19:59 -0400

    MK-954 Fix file description display

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -7719,6 +7719,10 @@ angular.module('obiba.mica.fileBrowser')
         return FileBrowserService.getLocalizedValue(values, ngObibaMicaFileBrowserOptions.locale);
       };
 
+      var hasLocalizedValue = function(values) {
+        return FileBrowserService.hasLocalizedValue(values, ngObibaMicaFileBrowserOptions.locale);
+      };
+
       $scope.downloadTarget = ngObibaMicaFileBrowserOptions.downloadInline ? '_blank' : '_self';
       $scope.getDownloadUrl = FileBrowserDownloadService.getUrl;
       $scope.screen = $rootScope.screen;
@@ -7735,6 +7739,7 @@ angular.module('obiba.mica.fileBrowser')
       $scope.isFile = FileBrowserService.isFile;
       $scope.isRoot = FileBrowserService.isRoot;
       $scope.getLocalizedValue = getLocalizedValue;
+      $scope.hasLocalizedValue = hasLocalizedValue;
       $scope.hideDetails = hideDetails;
       $scope.showDetails = showDetails;
       $scope.getTypeParts = getTypeParts;
@@ -7837,6 +7842,11 @@ angular.module('obiba.mica.fileBrowser')
         });
 
         return result && result.length > 0 ? result[0].value : null;
+      };
+
+      this.hasLocalizedValue = function (values, lang) {
+        var value = this.getLocalizedValue(values, lang);
+        return value !== null && value.trim().length > 0;
       };
 
       this.getDocumentIcon = function (document) {
@@ -8500,14 +8510,13 @@ angular.module("file-browser/views/document-detail-template.html", []).run(["$te
     "        </div>\n" +
     "      </div>\n" +
     "\n" +
-    "      <div ng-init=\"desc = getLocalizedValue(data.details.document.attachment.description)\"\n" +
-    "           ng-show=\"desc\"\n" +
+    "      <div ng-show=\"hasLocalizedValue(data.details.document.attachment.description)\"\n" +
     "           class=\"voffset2\">\n" +
     "        <label class=\"text-muted no-margin\">\n" +
     "          <small>{{'description' | translate}}</small>\n" +
     "        </label>\n" +
     "        <div>\n" +
-    "          <span>{{desc}}</span>\n" +
+    "          <span>{{getLocalizedValue(data.details.document.attachment.description)}}</span>\n" +
     "        </div>\n" +
     "      </div>\n" +
     "    </div>\n" +
