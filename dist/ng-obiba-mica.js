6ef92f595fe54247a6d0b3a0661145e5ec09aced
Cedric Fontin <cfontin@maelstrom-research.org>
2016-10-06 13:04:29 -0400

    MLSTRM-421 - add chartAggregationName to watchGroup
    Conflicts:
    	dist/ng-obiba-mica.min.js

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -3,7 +3,7 @@
  * https://github.com/obiba/ng-obiba-mica
 
  * License: GNU Public License version 3
- * Date: 2016-10-05
+ * Date: 2016-10-06
  */
 'use strict';
 
@@ -6968,6 +6968,7 @@ angular.module('obiba.mica.graphics')
                   }
                 });
 
+
                 $scope.updateCriteria = function(key, vocabulary) {
                   RqlQueryService.createCriteriaItem('study', 'Mica_study', vocabulary, key).then(function (item) {
                     var entity = GraphicChartsConfig.getOptions().entityType;
@@ -7040,8 +7041,8 @@ angular.module('obiba.mica.graphics')
         $scope.ready = true;
       });
 
-      $scope.$watchGroup(['chartType', 'ready'], function() {
-        if ($scope.chartType && $scope.ready) {
+      $scope.$watch('chartAggregationName', function() {
+        if ($scope.chartAggregationName) {
           initializeChartData();
         }
       });
