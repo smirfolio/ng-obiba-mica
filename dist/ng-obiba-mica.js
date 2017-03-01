a926c65cdd302c304ab0ddb7854ca23f4863e0ff
Romain Roces <romain.roces+github@gmail.com>
2017-03-01 09:25:23 -0500

    Ignore special characters on search (free-text)

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -4964,7 +4964,12 @@ angular.module('obiba.mica.search')
       };
 
       var onSelectTerm = function (target, taxonomy, vocabulary, args) {
+
         args = args || {};
+
+        if (args.text) {
+          args.text = args.text.replace(/[^a-zA-Z0-9 _-]/g, '');
+        }
 
         if(angular.isString(args)) {
           args = {term: args};
