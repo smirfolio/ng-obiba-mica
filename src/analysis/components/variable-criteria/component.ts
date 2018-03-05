b6d7002965a983b9a1505abd5d0b458580db667a
Yannick Marcon <yannick.marcon@obiba.org>
2018-03-05 21:26:20 +0100

    MK-1796 Numerical variables can be filtered by exact values

--- src/analysis/components/variable-criteria/component.ts
+++ src/analysis/components/variable-criteria/component.ts
@@ -43,8 +43,10 @@ class VariableCriteriaController implements ng.IComponentController {
   public searchText: string;
   public selectedCategories: any;
   public selectedOperation: string;
-  public selectedMin: number;
-  public selectedMax: number;
+  public selectedNumericalOperation: string;
+  public selectedNumericalMin: number;
+  public selectedNumericalMax: number;
+  public selectedNumericalValues: string;
 
   constructor(
     private VariableResource: any,
@@ -62,35 +64,13 @@ class VariableCriteriaController implements ng.IComponentController {
 
   public $onInit() {
     this.loading = true;
-    this.rqlQuery = this.parseQuery();
-    if (this.rqlQuery.args) {
-      const isNot = this.rqlQuery.name === "not";
-      const rqlQueryWithArgs = isNot ? this.rqlQuery.args[0] : this.rqlQuery;
-      // get variable from field name
-      this.id = rqlQueryWithArgs.args[0].join(":");
-      this.VariableResource.get({ id: this.id }, this.onVariable(), this.onError());
-      // get categories if any
-      if (rqlQueryWithArgs.args.length > 1) {
-        if (rqlQueryWithArgs.name === "in") {
-          rqlQueryWithArgs.args[1].forEach((value) => {
-            this.selectedCategories[value] = true;
-          });
-        } else if (rqlQueryWithArgs.name === "range" && rqlQueryWithArgs.args[1].length > 0) {
-          this.selectedMin = rqlQueryWithArgs.args[1][0] === "*" ? undefined : rqlQueryWithArgs.args[1][0];
-          this.selectedMax = rqlQueryWithArgs.args[1].length < 2 || rqlQueryWithArgs.args[1][1] === "*" ?
-            undefined : rqlQueryWithArgs.args[1][1];
-        }
-      }
-      this.selectedOperation = this.rqlQuery.name === "range" ? "in" : this.rqlQuery.name;
-      if (isNot && (rqlQueryWithArgs.name === "in" || rqlQueryWithArgs.name === "range")) {
-        this.selectedOperation = "out";
-      }
-      if (isNot && rqlQueryWithArgs.name === "exists") {
-        this.selectedOperation = "empty";
-      }
-    }
+    this.initializeState();
   }
 
+  /**
+   * Close on return submit and escape. Submit the update request only on return.
+   * @param event event to get the key
+   */
   public onKeyup(event): void {
     if (event.keyCode === 13) {
       this.closeDropdown(false);
@@ -99,6 +79,10 @@ class VariableCriteriaController implements ng.IComponentController {
     }
   }
 
+  /**
+   * Applies the categories filter text: visible categories are the ones which label matches the searched text.
+   * @param event ignored
+   */
   public onSearchTextKeyup(event): void {
     const filter = this.searchText.trim();
     if (this.categoriesData)  {
@@ -109,53 +93,39 @@ class VariableCriteriaController implements ng.IComponentController {
     }
   }
 
+  /**
+   * Make sure numerical values are space separated numbers.
+   * @param event ignored
+   */
+  public onNumericalValuesKeyup(event): void {
+    this.$log.info(this.selectedNumericalValues);
+    let values = "";
+    if (this.selectedNumericalValues) {
+      for (let i = 0; i < this.selectedNumericalValues.length; i++) {
+        const c = this.selectedNumericalValues.charAt(i);
+        if (c === " " || c === "." || c === "-" || !isNaN(parseInt(c, 10))) {
+          values = values + c;
+        }
+      }
+      this.selectedNumericalValues = values;
+    }
+  }
+
+  /**
+   * Close the dropdown and submit update request if changes are detected.
+   * @param cancel cancel update request if true
+   */
   public closeDropdown(cancel: boolean) {
     this.state.open = false;
     if (cancel) {
       return;
     }
-    // get the query from the selections
-    let newQuery = "";
-    let args;
-    if (this.showCategoricalOptions()) {
-      args = Object.keys(this.selectedCategories).filter((key) => {
-        return this.selectedCategories[key];
-      }).map((key) => key).join(",");
-    }
-    if (this.showNumericalOptions()) {
-      const min = this.selectedMin ? this.selectedMin : "*";
-      const max = this.selectedMax ? this.selectedMax : "*";
-      args = [min, max].join(",");
-    }
-    switch (this.selectedOperation) {
-      case Operation.All:
-      case Operation.Exists:
-        newQuery = this.selectedOperation + "({field})";
-        break;
-      case Operation.Empty:
-        newQuery = "not(exists({field}))";
-        break;
-      case Operation.In:
-      if (args && args.length > 0) {
-        newQuery = this.getNature() === "CATEGORICAL" ? "in({field},({args}))" : "range({field},({args}))";
-      } else {
-        newQuery = "not(exists({field}))";
-        this.selectedOperation = "empty";
-      }
-      break;
-      case Operation.Out:
-      if (args && args.length > 0) {
-        newQuery = this.getNature() === "CATEGORICAL" ? "not(in({field},({args})))" : "not(range({field},({args})))";
-      } else {
-        newQuery = "exists({field})";
-        this.selectedOperation = "exists";
-      }
-    }
-    newQuery = newQuery.replace("{field}", this.variable.id);
-    newQuery = newQuery.replace("{args}", args);
-    this.onUpdate(newQuery);
+    this.update(this.makeNewQuery());
   }
 
+  /**
+   * Open the dropdown.
+   */
   public openDropdown() {
     if (this.state.open) {
       this.closeDropdown(false);
@@ -164,22 +134,17 @@ class VariableCriteriaController implements ng.IComponentController {
     this.state.open = true;
   }
 
+  /**
+   * Removes the criteria.
+   */
   public onRemove(): void {
-    this.onUpdate("");
+    this.update("");
   }
 
-  public onUpdate(newQuery: string): void {
-    if (this.query === newQuery) {
-      return;
-    }
-    const search = this.$location.search();
-    search.query = search.query.split(this.query).join("").replace(/,,/, ",").replace(/^,/, "").replace(/,$/, "");
-    if (newQuery && newQuery.length !== 0) {
-        search.query = search.query + "," + newQuery;
-    }
-    this.$location.search(search);
-  }
-
+  /**
+   * Get a human readable translated label of the criteria query.
+   * @param truncated true if the title is to be truncated
+   */
   public getQueryTitle(truncated: boolean): string {
     if (!this.rqlQuery) {
       return "?";
@@ -202,32 +167,106 @@ class VariableCriteriaController implements ng.IComponentController {
     return truncated && title.length > 50 ? title.substring(0, 50) + "..." : title;
   }
 
+  /**
+   * Check whether the categorical options are to be shown.
+   */
   public showCategoricalOptions(): boolean {
     return this.getNature() === "CATEGORICAL" && this.showOptions();
   }
 
+  /**
+   * Check whether the numerical options are to be shown.
+   */
   public showNumericalOptions(): boolean {
     return this.getNature() === "CONTINUOUS" && this.isNumerical() && this.showOptions();
   }
 
+  /**
+   * Parse the query and initialize the component state with variable information.
+   */
+  private initializeState(): void {
+    this.rqlQuery = this.parseQuery();
+    if (this.rqlQuery.args) {
+      const isNot = this.rqlQuery.name === "not";
+      const rqlQueryWithArgs = isNot ? this.rqlQuery.args[0] : this.rqlQuery;
+      // get variable from field name
+      this.id = rqlQueryWithArgs.args[0].join(":");
+      this.VariableResource.get({ id: this.id }, this.onVariable(), this.onError());
+      // get categories if any
+      if (rqlQueryWithArgs.args.length > 1) {
+        if (rqlQueryWithArgs.name === "in") {
+          // don't know at this point if it is a categorical or numerical variable
+          rqlQueryWithArgs.args[1].forEach((value) => {
+            this.selectedCategories[value] = true;
+          });
+          this.selectedNumericalOperation = "in";
+          this.selectedNumericalValues = rqlQueryWithArgs.args[1].filter((val) => !isNaN(val)).join(" ");
+        } else if (rqlQueryWithArgs.name === "range" && rqlQueryWithArgs.args[1].length > 0) {
+          this.selectedNumericalOperation = "range";
+          this.selectedNumericalMin = rqlQueryWithArgs.args[1][0] === "*" ? undefined : rqlQueryWithArgs.args[1][0];
+          this.selectedNumericalMax = rqlQueryWithArgs.args[1].length < 2 || rqlQueryWithArgs.args[1][1] === "*" ?
+            undefined : rqlQueryWithArgs.args[1][1];
+        }
+      }
+      this.selectedOperation = this.rqlQuery.name === "range" ? "in" : this.rqlQuery.name;
+      if (isNot && (rqlQueryWithArgs.name === "in" || rqlQueryWithArgs.name === "range")) {
+        this.selectedOperation = "out";
+      }
+      if (isNot && rqlQueryWithArgs.name === "exists") {
+        this.selectedOperation = "empty";
+      }
+    }
+  }
+
+  /**
+   * Replace the original query by the new one. If query is empty, the criteria is to be removed.
+   * @param newQuery critera query
+   */
+  private update(newQuery: string): void {
+    if (this.query === newQuery) {
+      return;
+    }
+    const search = this.$location.search();
+    search.query = search.query.split(this.query).join("").replace(/,,/, ",").replace(/^,/, "").replace(/,$/, "");
+    if (newQuery && newQuery.length !== 0) {
+        search.query = search.query + "," + newQuery;
+    }
+    this.$location.search(search);
+  }
+
+  /**
+   * Check if none of the global operations are selected (all, exists, empty).
+   */
   private showOptions(): boolean {
     return Operation.All !== this.selectedOperation
       && Operation.Exists !== this.selectedOperation
       && Operation.Empty !== this.selectedOperation;
   }
 
+  /**
+   * Get the variable's nature.
+   */
   private getNature(): string {
     return this.variable ? this.variable.nature : "?";
   }
 
+  /**
+   * Check if the variable has a numerical type (integer or decimal).
+   */
   private isNumerical(): boolean {
     return this.variable.valueType === "integer" || this.variable.valueType === "decimal";
   }
 
+  /**
+   * Check if the variable has a logical type (boolean).
+   */
   private isLogical(): boolean {
     return this.variable.valueType === "boolean";
   }
 
+  /**
+   * Get the translated label of the criteria query operation.
+   */
   private getOperationTitle(): string {
     const rqlQueryWithArgs = this.getRqlQueryWithArgs();
     if (this.isNotQuery()) {
@@ -240,6 +279,9 @@ class VariableCriteriaController implements ng.IComponentController {
     return this.$filter("translate")("analysis." + (rqlQueryWithArgs.name === "range" ? "in" : rqlQueryWithArgs.name));
   }
 
+  /**
+   * Get the RQL query node that contains the criteria arguments.
+   */
   private getRqlQueryWithArgs(): any {
     if (this.isNotQuery()) {
       return this.rqlQuery.args[0];
@@ -247,10 +289,16 @@ class VariableCriteriaController implements ng.IComponentController {
     return this.rqlQuery;
   }
 
+  /**
+   * Check if the RQL query node is a 'not' operation.
+   */
   private isNotQuery(): boolean {
     return this.rqlQuery && this.rqlQuery.name === "not";
   }
 
+  /**
+   * Parse the query string as a RQL query node.
+   */
   private parseQuery() {
     try {
       return new RqlParser().parse(this.normalize(this.query)).args[0];
@@ -260,10 +308,19 @@ class VariableCriteriaController implements ng.IComponentController {
     return new RqlQuery();
   }
 
+  /**
+   * Variable identifier separator is not valid for the RQL parser: using path separator, the RQL parser will
+   * split the variable identifier tokens automatically.
+   * @param str variable identifier
+   */
   private normalize(str: string): string {
     return str.split(":").join("/");
   }
 
+  /**
+   * Get the translated label of a variable category.
+   * @param value category name
+   */
   private localizeCategory(value: string): string {
     if (!this.variable.categories) {
       return this.isLogical() ? this.$filter("translate")("global." + value) : value;
@@ -284,10 +341,17 @@ class VariableCriteriaController implements ng.IComponentController {
     return label || value;
   }
 
+  /**
+   * Extract the translation for the current language.
+   * @param values labels object
+   */
   private localize(values): string {
     return this.LocalizedValues.forLang(values, this.$translate.use());
   }
 
+  /**
+   * Extract the categories from the variable object.
+   */
   private prepareCategories(): void {
     this.categoriesData = [];
     if (this.getNature() === "CATEGORICAL") {
@@ -315,6 +379,68 @@ class VariableCriteriaController implements ng.IComponentController {
     }
   }
 
+  /**
+   * Get the new query from the selections.
+   */
+  private makeNewQuery(): string {
+    let newQuery = "";
+    let args;
+    if (this.showCategoricalOptions()) {
+      args = Object.keys(this.selectedCategories).filter((key) => {
+        return this.selectedCategories[key];
+      }).map((key) => key).join(",");
+    }
+    if (this.showNumericalOptions()) {
+      if (this.selectedNumericalOperation === "range") {
+        const min = this.selectedNumericalMin ? this.selectedNumericalMin : "*";
+        const max = this.selectedNumericalMax ? this.selectedNumericalMax : "*";
+        args = [min, max].join(",");
+      } else {
+        args = this.selectedNumericalValues.split(" ").join(",");
+      }
+    }
+    switch (this.selectedOperation) {
+      case Operation.All:
+      case Operation.Exists:
+        newQuery = this.selectedOperation + "({field})";
+        break;
+      case Operation.Empty:
+        newQuery = "not(exists({field}))";
+        break;
+      case Operation.In:
+      if (args && args.length > 0) {
+        if (this.showCategoricalOptions()
+          || (this.showNumericalOptions() && this.selectedNumericalOperation === "in")) {
+          newQuery = "in({field},({args}))";
+        } else if (this.showNumericalOptions() && this.selectedNumericalOperation === "range") {
+            newQuery = "range({field},({args}))";
+        }
+      } else {
+        newQuery = "not(exists({field}))";
+        this.selectedOperation = "empty";
+      }
+      break;
+      case Operation.Out:
+      if (args && args.length > 0) {
+        if (this.showCategoricalOptions()
+          || (this.showNumericalOptions() && this.selectedNumericalOperation === "in")) {
+          newQuery = "not(in({field},({args})))";
+        } else if (this.showNumericalOptions() && this.selectedNumericalOperation === "range") {
+            newQuery = "not(range({field},({args})))";
+        }
+      } else {
+        newQuery = "exists({field})";
+        this.selectedOperation = "exists";
+      }
+    }
+    newQuery = newQuery.replace("{field}", this.variable.id);
+    newQuery = newQuery.replace("{args}", args);
+    return newQuery;
+  }
+
+  /**
+   * Variable response processor.
+   */
   private onVariable() {
     const that = this;
     return (response: any) => {
@@ -325,6 +451,9 @@ class VariableCriteriaController implements ng.IComponentController {
     };
   }
 
+  /**
+   * Variable summary response processor.
+   */
   private onVariableSummary() {
     const that = this;
     return (response: any) => {
@@ -354,6 +483,9 @@ class VariableCriteriaController implements ng.IComponentController {
     };
   }
 
+  /**
+   * General error handler (to be improved).
+   */
   private onError() {
     const that = this;
     return (response: any) => {
