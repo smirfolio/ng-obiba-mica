d54e9189bd53001ee47cd71afb40653e4d3cad73
samirMr <sbensghaier@maelstrom-research.org>
2019-06-21 10:29:36 -0400

    Prepare release

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -1,9 +1,9 @@
 /*!
- * ng-obiba-mica - v3.6.0
+ * ng-obiba-mica - v3.6.1
  * https://github.com/obiba/ng-obiba-mica
  *
  * License: GNU Public License version 3
- * Date: 2019-06-17
+ * Date: 2019-06-21
  */
 /*
  * Copyright (c) 2018 OBiBa. All rights reserved.
