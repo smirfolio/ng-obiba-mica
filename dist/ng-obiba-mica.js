37c1c22300f6fa22ae15260add10e0108cf0a233
meek0 <cedricgfont@gmail.com>
2019-06-25 09:16:59 -0400

    dist

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -3,7 +3,7 @@
  * https://github.com/obiba/ng-obiba-mica
  *
  * License: GNU Public License version 3
- * Date: 2019-06-21
+ * Date: 2019-06-25
  */
 /*
  * Copyright (c) 2018 OBiBa. All rights reserved.
