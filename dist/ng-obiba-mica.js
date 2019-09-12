6c0464a139023b29bf3b223b50d6ac0bd26ed7ee
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2019-09-12 16:36:21 -0400

    Updated dist/

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -5948,6 +5948,7 @@ var TaxonomyCartFilter = /** @class */ (function () {
                     validateBucket(bucket);
                     validateDisplay(display);
                     $scope.search.type = type;
+                    $scope.search.withZeros = search.withZeros;
                     $scope.search.bucket = bucket;
                     $scope.search.display = display;
                     $scope.search.query = query;
