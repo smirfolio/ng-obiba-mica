cebdfc2ed77dbb35b4959f49e2cb1cdf477b492a
samirMr <sbensghaier@maelstrom-research.org>
2019-12-11 16:32:20 -0500

    Make sure to don't display graphs if no corresponded taxonomy

--- src/search/components/result/graphics-result/component.js
+++ src/search/components/result/graphics-result/component.js
@@ -7,12 +7,15 @@ ngObibaMica.search
     'RqlQueryService',
     '$filter',
     '$scope',
-
+    'TaxonomyResource',
+    'VocabularyService',
     function (GraphicChartsConfig,
       GraphicChartsUtils,
       RqlQueryService,
       $filter,
-      $scope
+      $scope,
+      TaxonomyResource,
+      VocabularyService
       ) {
       $scope.updateCriteria = function (key, vocabulary) {
         RqlQueryService.createCriteriaItem('study', 'Mica_study', vocabulary, key).then(function (item) {
@@ -30,16 +33,34 @@ ngObibaMica.search
 
        var graphs = GraphicChartsUtils.getGraphConfig();
 
-        if (result && result.studyResultDto.totalHits) {
-          $scope.noResults = false;
-          $scope.dtosResult = result;
-          $scope.chartObjects = [];
-          Object.keys(graphs).forEach(function(Chart){
-            var chartOption = {
-              chartConfig: null
+        var studyTaxonomy = {};
+        if(!studyTaxonomy.vocabularies){
+          TaxonomyResource.get({
+            target: 'study',
+            taxonomy: 'Mica_study'
+          }).$promise.then(function(taxonomy){
+            studyTaxonomy.vocabularies = angular.copy(taxonomy.vocabularies);
+            var haveTaxonomy = function(aggregationName){
+              return studyTaxonomy.vocabularies.find(function(vocabulary){
+                if(VocabularyService.vocabularyAlias(vocabulary) === aggregationName){
+                  return true;
+                }
+              });
             };
-            chartOption.chartConfig = graphs[Chart];
-            $scope.chartObjects.push(chartOption);
+            if (result && result.studyResultDto.totalHits) {
+              $scope.noResults = false;
+              $scope.dtosResult = result;
+              $scope.chartObjects = [];
+              Object.keys(graphs).forEach(function(Chart){
+                var chartOption = {
+                  chartConfig: null
+                };
+                if(haveTaxonomy(graphs[Chart].chartAggregationName)){
+                  chartOption.chartConfig = graphs[Chart];
+                  $scope.chartObjects.push(chartOption);
+                }
+              });
+            }
           });
           $scope.dtosResult = result;
         }
