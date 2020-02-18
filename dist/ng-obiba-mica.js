4192a7677247cf8bfcdf5cc56da1071ce93b82a0
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2020-02-18 12:26:27 -0500

    Updated dist/

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -9466,7 +9466,7 @@ ngObibaMica.search.service("VariableAnnotationsService", VariableAnnotationsServ
             return vocabularyAttributeValue(vocabulary, 'termsSortKey', null);
         }
         function isTermsVocabulary(vocabulary) {
-            return vocabularyType(vocabulary) === VOCABULARY_TYPES.KEYWORD && vocabulary.terms;
+            return [VOCABULARY_TYPES.KEYWORD, VOCABULARY_TYPES.STRING].indexOf(vocabularyType(vocabulary)) > -1 && vocabulary.terms;
         }
         function isMatchVocabulary(vocabulary) {
             return vocabularyType(vocabulary) === VOCABULARY_TYPES.STRING && !vocabulary.terms;
